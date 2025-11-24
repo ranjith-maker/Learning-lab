19298e48b6d76b14c7c292ac980d28bc80ded134
snranjith500@gmail.com <snranjithkumar500@gmail.com>
2025-11-24 21:57:50 +0530

    Learning Loop-1

---
+++ first.js
@@ -0,0 +1,141 @@
+
+// Learing Loop
+// let i = 1;
+// while( i<6)
+// {
+// console.log(i)
+// i++
+// }
+//While Loop ---> start while(end) code change 
+// let i = 1;
+// while(i < 33) 
+// {
+//  console.log(i) 
+// i++
+// }
+
+// start
+// do{
+//     code
+//     change
+// }
+// end
+
+
+// let i = 12;
+// do{
+//     console.log(i)
+//     i++
+// }
+// while(i<20)
+
+// let i = 12;
+// while (i < 20)
+// {
+// console.log(i)
+// i++
+// }
+
+//Break in Loop
+
+// for(let i =1; i<50; i++ ){
+//     console.log(i);
+//     if (i === 32){
+//         break; 
+//     }
+// }
+//It breaks at 32
+
+//Continue in Loop
+// for (let i = 0;   i<20; i++){
+//   if(i ===15) {
+//       continue;}  
+//     console.log(i)
+// }
+
+
+
+// for(i=1; i<=20; i++){
+// if(i===15){
+//     continue; 
+// }
+// console.log(i)
+// }
+
+//Print numbers 1-10 from using for loop
+// for(i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// Print numbers 10-1 from using a while loop
+// let i=10;
+// while(i>0)
+// {
+// console.log(i)    
+// i--
+// }
+
+//3- Print even numbers from 1 to 20 using for loop
+// for(let i = 1; i<=50; i+=2){
+//     console.log(i)
+// }
+
+// 4- Print odd numbers from 1 to 15 using from whileloop
+
+// let i = 1;
+// while( i<16){
+//     console.log(i)
+//     i+=2
+//}
+
+// for(let i= 0; i<20; i+=2){
+//     console.log(i) 
+// }
+
+// for(let i = 1; i<21; i++){
+// if( i%2 === 0){
+// console.log(i)
+// }
+// }
+
+
+
+
+// for(let i = 1; i<20; i++ ){
+//     if(i%2 === 1){
+//         console.log(i)
+//     }
+// }
+
+
+
+// for(let i = 0; i<10; i+=2){
+//     console.log(i)
+// }
+
+// for(let i = 1; i<11; i++){
+// console.log( `5 * ${i} = ${i}`)
+// }
+
+
+
+// let sum =0;
+// for(let i = 1; i<101; i++){
+//     sum += i
+// }
+// console.log(sum)
+
+
+
+// sum = 0;
+// let i = 1;
+// while(i<101){
+//     sum+=i
+//      i++
+// }
+// console.log(sum)    
+
+
+
+
+
