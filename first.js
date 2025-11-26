2cccd2bd941a0c5ae68e86759aec1a425302b40e
snranjith500@gmail.com <snranjithkumar500@gmail.com>
2025-11-26 22:02:43 +0530

    HOF,filters

--- first.js
+++ first.js
@@ -202,231 +202,444 @@
 // com(12,13,54,68,39,73,687,7129) ---> 12 13 54 68 39 73 687 7129
 
 
-// function abc(val){
-//     return 12 + val;
-// }
-// let value =abc(10);   
-// console.log(value) ---> 22
-
-// function abc(){  
-//     return 12;
-// }
-// let value = abc();
-// console.log(value)  ---> 12
-
-// FOR OF LOOP
-// let arr = [ 10,20,30,40,50,60 ];
-// for(let key of arr){
-// console.log(key)    
-// }   -->102030405060 it prints all the array
-
-// let str = "Rohit is a good boy"
-// for( let key of str){
-// console.log(key)    
-// } ---> it prints the string in single single letter one one 
-
-// if you have to iterate over Object you can turn into an array 
-// first, whuc  we easily do by accessign Object's value or keys 
-
-// let obj = {
-//     name :"Tara",
-//     age :40,
-//     city :"Tokyo"
-// };
-
-// for(let key of Object.keys(obj)){
-//     console.log(key, obj[key])
-// }
-// for(let key of Object.keys(obj) ){
-//     console.log(key)// --> name,age,city
-// }
-// for(let key of Object.values(obj)){
-//     console.log(key) //---> Tanya, 40, Tokyo
+// let opj = {
+//     name : "Rohan",
+//     age : 20,
+//     city : "Mumbai",
+//     balance : 45000
 // }
 
-// let fruits = ["Apple", "Banana", "Mango"];
-//  for(let key of fruits){
-//     console.log(key) 
+// // for (let key in opj){
+// //     console.log(key,opj[key])
+// // }
+
+// let opj3 = Object.create(opj);
+// opj3.name = "Rakesh";
+// opj3.state = "Maharashtra";
+
+// // console.log(opj3)
+// for(let kay in opj3 ){
+//   console.log(kay,opj3[kay]  )
+    
+// } 
+
+
+// function names(dan){
+//     console.log("Hello all")
+//     dan();
 // }
 
-
-//Call Back Function
-
-//you make one function that fetches the backedn data
-
-// function fetchData(){
-//     console.log("I am fetching data")
+// function greet(){
+// console.log( "Hey people" )
 // }
-// setInterval(fetchData,
 
+// names(greet)
 
-// Let's your website has to call API and update information every
-// 5s. you can call setInterval () it takes 2 values, what value
-// and how oftern it needs to change, 5000,write in millisecond 
+//Second type of function
 
-// In For each we use the second type of callback,that, calling a
-// function inside writing another whole whole function
+// function names(san){
+//   console.log("Hello")
+//   san();
+// }
 
-// let arr = [10,20,30,40,50  ];
-// arr.forEach(function(num) {
-//     console.log(num)
-// }) ---> 10...50
-
-
-
-// let arr =[ 10,20,30,40,50  ]
-// arr.forEach( (num)=> console.log(num))  // --> 10...50
-
-// let arr = [10,20,30,40,50];
-// arr.forEach(num=>console.log(num))
-
-// let arr = [10,20,30,40,50]
-// arr.forEach
-
-
-// arr.forEach(num=>console.log(num))
-
-
-// we can pass 3 arguments in forEach loop
-// first -number-> which is Array's value
-// second -index --> which gives index no. also
-// third - manipulation
-// we saw the first, let see the second 
-
-// let arr = [10,20,30,40,50];
-// arr.forEach((a, ab,arr)=>{
-//  arr[ab] = a*2
-
+// names(function(good){
+//   console.log("Heyyyyyyyyyyy")
 // })
 
-// console.log(arr)  ---> [ 20, 40, 60, 80, 100 ]
-
-// It starts with variable name.forEach here arr.forEach
-// Inside foreach loop there are 3 elements we can store,first 2 
-// is anything last is array's variable
-// first value represents --> a - basic array's value 10 20..50
-// second value represents --> ab - it gives of each index with
-// array as it comes after first value so even in ans --> value
-// index 10 0  
-// third value repersents ---> arr your arrya's variable, if you
-// want to manipulate it you can do it, like multiply, add, sub if
-// you need to multiply with 2, do this in the next line, next
-// line is, it needs to comes as a reverse ex, 
-// arr[ab] = a * 2
-// 3rd[2nd] = 1st changes 
-
-// after closing the bracket, console need to come outside the
-// bracket to print it all 3, first two anything and arrays's
-// variable 
-
-// Printing Array's values
-// let arr = [10,20,30,40,50];
-// arr.forEach(a => {
-//     console.log(a)
-//   })   ---> 10 20 30 40 50
 
 
-// Printing Array's values and index 
-// let arr = [10,20,30,40,50];
-//  arr.forEach((a,ab)=>{
-//      console.log(a,ab)
-//  } )  ---> 10 0 20 1
- 
-//  Array with manipulation 
-// let arr = [10,20,30,40,50];
-// arr.forEach((a,ab,arr)=>{
-// arr[ab] = a*2;
+
+//For Each Loop 
+
+// let are = [10,20,30,40,50];
+
+// are.forEach(function(key){
+//     console.log(key)
 // })
-// console.log(arr) --> [ 20, 40, 60, 80, 100 ]
 
 
-
-// let ary = [10,20,30,40,50];
-// // Use forEach to get the value 
-// // here we can use call back
-
-// let are = function(num){
-//     console.log(num)
-// }
-// ary.forEach(are) ---> 10 20 30 40 50 
-
-// Mostly we get cases like 1st one, taking array values with the
-// help of forEach
-
-
-//JS - Filter
-
-// let arr = [10,20,31,47,50];
-// let result = arr.filter((no) => no%2 === 0);
-// console.log(result)
-//like callback function we don't need {} and return 
-
-
-// let are = [21,11,65,40,13,11];
-
-// let result = are.filter((nos)=>{
-//     return nos%2 === 1;
+// let are = [10,20,30,40,50];
+// are.forEach((nos,ind,are)=>{
+    
+//     are[ind] = nos  * 2
 // })
-// console.log(result) ---> [ 21, 11, 65, 13, 11 ]
+// console.log(are)
 
 
+// Filter even no.s
+// let arr = [10,20,45,79,46,13,49,66,78]
 
-// let result = ary.filter((val) => val%2 === 1);
-// console.log(result) ---> [ 29, 31, 47 ]
- 
-// let ary = [10,29,31,47,50];
-// let result = ary.filter((vel)=> vel%2 === 1);
+// let result = arr.filter((arr)=>{
+//     return arr%2 === 0
+// })
 // console.log(result)
 
 
 
-// let arr = [10, 29, 31, 47, 50];
-// let result = [];
+// let arr = [10,20,45,79,46,13,49,66,78]
+// let result = arr.filter(function(x){
+//     return x%2 === 0
+// });
+// console.log(result)
 
-// for (let i = 0; i < arr.length; i++) {
-//   if (arr[i] % 2 === 1) {
-//     result.push(arr[i]);
-//   }
+// function names(san){
+//   console.log("Hello")
+//   san();
 // }
-
-// console.log(result);
-
-
-
+// names(function(good){
+//   console.log("Heyyyyy")
+// })
 
 
+
+
+
+
+
+// let arr = [10,20,45,79,46,13,49,66,78]
+// let result = arr.filter((nos) => nos%2 ===1  
+// )
+// console.log(result)
+
+
+
+
+
+// Same done with for loop
 // let arr = [10, 29, 31, 47, 50];
-
 // let result = [];
 // for(let i =1; i<arr.length; i++ ){
 //     if(arr[i]%2  === 1)
 //     result.push(arr[i])
+// }
+// console.log(result) [ 29, 31, 47 ]
+
+
+// let arr = [10, 29, 31, 47, 50];
+
+// let sum = [];
+// for(let i = 1; i<arr.length; i++){
+//     if(arr[i]%2 === 1)
+//     sum.push(arr[i])
+// }
+
+// console.log(sum)
+//  const students = [
+
+// {name : "Rohan", age : 22, marks : 70 },
+// {name : "Mohan", age : 24, marks : 80 },
+// {name : "Darshan", age : 28, marks : 30 },
+// {name : "Mohit", age : 32, marks : 40 },
+// {name : "Shadik", age : 12, marks : 90 },
+// ]
+
+
+// let result = students.filter((val)=> val.marks>50);
+// console.log(result)
+
+
+// let result = students.filter(({marks})=> marks>50);
+// console.log(result)
+
+// let arr = [10,20,30,40,50];
+// let result = arr.map((val)=>{
+//       return val*val
+// });
+// console.log(result) ---> [ 100, 400, 900, 1600, 2500 ]
+
+// Just as filter, after arr.map((val))--> this val takes all the
+// values of arr and arrowed to return val themselves into  *
+// values 
+
+// let arr = [10,20,30,40,50];
+// let result = arr.map((val)=>val * val);
+// console.log(result)
+
+
+
+// let arr =[1,2,3,4,5,6];
+// let result = arr.filter((val)=> val%2 === 0).map((num)=> num
+// *num).map((nom)=> nom/2);
+// console.log(result)
+
+// Here we've both the filer and map 
+// Filter - here we've filtered the even no.
+// Map - we have squared the filtered value as the ans it printed
+// the final ans as squared one
+
+
+
+
+// function callsback(callbackFunc) {
+//     callbackFunc()
+// }
+
+// function firstClass() {
+//     console.log("I was called");
+// }
+
+// firstClass(); // "I was called"
+// callsback(firstClass); // "I was called"
+
+
+
+// function myCalculator(num1, num2) {
+//   let sum = num1 + num2;
+//   return sum;
+// }
+
+// let result = myCalculator(5, 5);
+// myDisplayer(result);
+
+
+// function myCalculator(num1, num2) {
+//   let sum = num1 + num2;
+//   myDisplayer(sum);
+// }
+
+// myCalculator(5, 5);
+
+
+// Higher Order Function
+// function abc(val){
     
 // }
-// console.log(result)
+// abc(function {
+    
+// })
 
-// Filter
-// let arr = [10, 29, 32, 47, 50];
-// let result = arr.filter((val) => val%2 ===0);
-// console.log(result)
+// Now the above fucntion has become a HOF, abc is called passed
+// the argument to the abc's parameter as another function,
+// now val has become another function, SO abc has become a HOF
+// ex - one who takes another work go higher, abc took a complete
+// function gave it to their own work so it is HOF 
+
+// function abc(val){
+//     return function (){
+//  console.log("Hello")       
+//     }
+// }
+// abc()()  -->Hello
 
 
-// Students Grade 
-const students = [
+// let a = 12;
+// function hal(){
+//     console.log("Hello")
+// }
+// hal()
+// this has done no changes to the outer a value so it is Pure
+// function
 
-{name : "Rohan", age : 22, marks : 70 },
-{name : "Mohan", age : 24, marks : 80 },
-{name : "Darshan", age : 28, marks : 30 },
-{name : "Mohit", age : 32, marks : 40 },
-{name : "Shadik", age : 12, marks : 90 },
-]
+// let a = 12;
+// function hol(){
+//     a++
+// console.log(a)
+    
+// }
+// hol()
+// This is a impure function where it changes the outer values,
+// if you call hol() it shows 13
 
-const result = students.filter((val)=> val.marks>50);
-console.log(result)
-ans - 
-[
-  { name: 'Rohan', age: 22, marks: 70 },
-  { name: 'Mohan', age: 24, marks: 80 },
-  { name: 'Shadik', age: 12, marks: 90 }
-]
+// function outer() {
+//     let outerVar = "I'm in the outer scope!";
+//     function inner() {
+//         console.log(outerVar); 
+//     }
+//     return inner;  
+// }
+// const closure = outer(); 
+// closure();
+
+// when you call a function ,for Closure keep this ans function
+// name inside a variable, and call with that variable for an
+// answer, It give us the access to the variable which is written
+// above the return,this return has an function and it'll print
+// the variable 
+
+
+// function out(){
+//     let abc = "Alphabetical";
+//     function inn (){
+//      console.log(abc)
+//      }
+// return inn
+// }
+
+// let control = out();
+// control()
+
+// control goes to out, out returns inn, inn is an function and it
+// prints abc which is a variable as ans 
+// when you call a function ,for Closure keep this ans function
+// name inside a variable, and call with that variable for an
+// answer, It give us the access to the variable which is written
+// above the return,this return has an function and it'll print
+// the variable 
+
+
+
+
+
+// let op1 = 5;
+// let op2 =10;
+
+// function num(){
+//     return op1 + op2;
+// }
+// console.log(num())
+
+
+// function abc(){
+//     let a = 10;
+// function def(){
+//     let b = 20;
+// function ghi(){
+//     let c = 30;
+// }    
+// }    
+// }
+// you can change a in abc all block, b in def, c in ghi 
+// b can't be accessed abc or outer , same to a b c as well, they
+// can be changed in their territory only
+
+
+//IIFE
+
+// (function (){
+//     console.log("This is IIFE")
+// }) (); //---> This is IIFE
+
+// crate a function with no name function (){}, keep all this in
+// () bracket and call it by (), it'll pritn whatever is inide the
+// function 
+
+
+// a()
+
+// let a = function(){
+//     console.log("hello")
+// }
+
+//  Hoisting works in declaration but in expression, it won't,
+//  what expression means if you store  it inside a variable, it
+//  won't print, it shows, it was it can't access before
+//  initialization
+
+// a()
+
+// let a = function(){
+//     console.log("hello")
+// }
+
+
+// let ab = (m1, m2) =>{
+//     return m1 + m2;}
+// console.log(ab(20,30))
+
+// let som= (no1, no2)=>{
+//   return no1 + no2;
+// }
+// console.log(som(50,30)) ---> 80
+
+
+// function greet(name = "Guest"){
+//     console.log("Hello", name)
+    
+// }
+
+// greet( "Rohith")  --> Hello Rohith
+
+
+// let total = 0;
+// function getscore(...score){
+//   score.forEach((val)=>{
+//       total = total+ val;
+//   })
+// return total;
+// }
+// console.log(getscore(10,20,40,30,50))  --> 150
+
+
+
+
+// let sum = 0;
+// for(let i =1; i<101; i++){
+//     sum += i;       
+// }
+// console.log(sum)
+
+
+
+
+
+// function checkAge(age){
+//  if(age<18) return "Too young"
+// return "Allowed"
+// }
+
+// console.log(checkAge(12))  ---> Too young
+// console.log(checkAge(18))  ---> Allowed
+
+
+// function nos(val){
+//     val();
+// }
+   
+// nos(function (){
+//     console.log("Hello")
+// }) ----> Hello
+
+
+
+// function kon(kya){
+//     console.log("Hello") 
+//     kya();
+// }
+// function kol(){
+//     console.log("Heyy") 
+// }
+
+// kon(kol)
+
+
+
+// function outer(){
+//     let count = 0;
+// return function(){
+//       count++ 
+//       console.log(count)
+// }    
+// }
+
+// let double = outer();
+// double()  --> 1
+// double() ---> 2
+
+
+// function out(){
+//     let greet = "Hello All"
+// return function(){
+//         console.log(greet)
+//     }
+// }
+// let welcome = out();
+// welcome() --> Hello All
+
+
+//Write a BMI Calculator 
+
+function BMI(weight, height){
+    return weight / (height * height)
+}
+console.log(BMI(50,1.5).toFixed(2)) ---> 22.22
+
+
+
+function checkAge(age){
+ if(age<18) return "Too young"
+return "Allowed"
+}
+
+console.log(checkAge(12))
+
 
